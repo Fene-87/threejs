cc7916ef8eb4e1ae137d6ffecb5fe68a04454710
Fene-87 <fenekayas87@gmail.com>
2023-08-03 23:18:34 +0300

    Create valtio state with default values

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import { headContentAnimation, headContainerAnimation, headTextAnimation, slideAnimation } from "../config/motion";
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home;
