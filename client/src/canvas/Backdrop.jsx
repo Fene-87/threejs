8161ac8c07d27cd6d5e394c278497355cb8d221b
Fene-87 <fenekayas87@gmail.com>
2023-08-04 23:42:03 +0300

    Make shirt bigger

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber';
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 // Background color behind shirt
 
 const Backdrop = () => {
   return (
-    <div>Backdrop</div>
+    <AccumulativeShadows>
+      <RandomizedLight />
+    </AccumulativeShadows>
   )
 }
 
