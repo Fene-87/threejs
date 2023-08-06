a5509571e837f1fd152daa5526534e4ee7e67c06
Fene-87 <fenekayas87@gmail.com>
2023-08-06 15:34:30 +0300

    Set activeFilterTab to update UI

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,6 +60,14 @@ const Customizer = () => {
         state.isLogoTexture = true;
         state.isFullTexture = false;
     }
+
+    // Set activeFilterTab to update UI
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName]
+      }
+    })
   }
 
   const readFile = (type) => {
@@ -109,8 +117,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick = {() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick = {() => handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
